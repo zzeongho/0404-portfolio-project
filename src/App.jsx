7b4b3781811a1bbb5ca642d71e7476da1590cafa
zzeongho <junghoo022@gmail.com>
2023-03-30 15:45:41 +0900

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import DogeFace from "./images/dogeFace.png";
+
 function App() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
-      <h2>hello</h2>
+    <div className="bg-red-100 min-h-screen">
+      <header className="bg-blue-100 h-12 md:h-20">
+        <div className="max-w-screen-xl mx-auto h-full flex justify-between items-center">
+          <div>zzeongho's Portfolio</div>
+          {/* <div>
+            <img className="w-12 left-0" src={DogeFace} alt="Doge"></img>
+          </div> */}
+          <ul className="flex gap-2 md:gap-8 text-xs sm:text-sm md:text-base">
+            <li className="bg-indigo-300 btn-style">Introduce</li>
+            <li className="bg-yellow-300 btn-style">Portfolio</li>
+            <li className="bg-green-300 btn-style">Contact Me</li>
+          </ul>
+        </div>
+      </header>
     </div>
   );
 }
